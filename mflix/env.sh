0134b118ce9330836a60af5752b8fb860cc697c8
Ziang Lu <ziangl@alumni.cmu.edu>
2020-01-08 17:32:43 +0800

    Minor updates

--- mflix/env.sh
+++ mflix/env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-user="zianglu"
-password="Zest2016!"
-db_name="mflix"
-export MFLIX_DB_URI="mongodb+srv://$user:$password@cluster0-hanbs.mongodb.net/$db_name?retryWrites=true&w=majority"
+export USER="zianglu"
+export PASSWORD="Zest2016!"
+export DB_NAME="mflix"
